e0f3d3008bc45193b7150ea799f1f53882b63c7b
theo-mandelbaum <mandelbaumtheo@gmail.com>
2024-10-14 14:20:20 -0400

    prep done

--- scripts/main.js
+++ scripts/main.js
@@ -16,18 +16,45 @@
 //        keys: an array of known valediction targets
 //    query: a constraint on which valediction targets to include. Only those of which query is a substring will be included.
 
+
 // get refs to the input and output elements in the page
 const input = document.getElementById("target");
 const output = document.querySelector("output");
 
+
 // when the input has focus and enter is pressed, invoke the function named later
 input.addEventListener("keydown", (ev) => {
-  console.debug("keydown", ev.key);
-  if (ev.key === "Enter") {
-    console.log("Enter detected. current value:", input.value);
-    // TODO use the provided later() function here
-  }
+ console.debug("keydown", ev.key);
+ if (ev.key === "Enter") {
+   console.log("Enter detected. current value:", input.value);
+   // TODO use the provided later() function here
+   if (input == "") {
+     options (displayButtons, input.value);
+   } else {
+     options (displayButtons);
+   }
+ }
 });
+
+
+const displayButtons = (keys) => {
+ keys.forEach(target => {
+   const ul = document.getElementById('available-targets');
+   const li = document.createElement('li');
+   const button = document.createElement('button');
+   button.textContent = target;
+
+
+   button.addEventListener('click', () => {
+     later (target, setOutput);
+   });
+
+
+   li.appendChild(button);
+   ul.appendChild(li);
+ });
+};
+
 
 // when you have the result from this function, update(replace) the content of the output element with the result formatted as:
 // "RESULT, TARGET" // where the all caps are placeholders for the corresponding values
@@ -36,15 +63,27 @@ input.addEventListener("keydown", (ev) => {
 // is "see you later", the output element should be updated to read:
 // see you later, alligator
 
+
 //
 const setOutput = (result) => {
-  console.log("setOutput", result);
-  // TODO see comments just above 🙄
+ console.log("setOutput", result);
+ // TODO see comments just above 🙄
+ output.innerHTML = result.valediction + ", " + result.target;
+ /*const ul = document.getElementById('available-targets');
+ const newLi = document.createElement("li");
+ newLi.textContent = result.valediction + ", " + result.target;
+ ul.appendChild(newLi);*/
 };
+
 
 // for Part 2
 // change the code so that rather than directly requesting a valediction with the user's input,
-// the page instead queries for matching targets using the provided options() function 
+// the page instead queries for matching targets using the provided options() function
 // (if the user hasn't entered anything, simply exclude the query argument in your invocation to options).
 // add each of the resulting target options as buttons in list items in the ul.
 // when any of these buttons are clicked, user the later() function to request the corresponding valediction and update the output element as in Part 1
+
+
+
+
+
